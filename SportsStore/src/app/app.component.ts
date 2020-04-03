4064730207e2dbcff798be6df79e56568ffb28e7
quyen wichers <quyen.wichers@gmail.com>
2020-04-03 07:03:02 -0700

    first commit

---
+++ SportsStore/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: "<store></store>"
+})
+export class AppComponent {
+  title = 'SportsStore';
+}
---
+++ SportsStore/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { StoreModule } from "./store/store.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
